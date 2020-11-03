b29fdffb42b8ac06a7e51e9c24dda8fc31922521
aimennyx <73884465+aimennyx@users.noreply.github.com>
2020-11-03 19:04:13 +0500

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -24,3 +24,19 @@ elif what == '/':
 else: 
 	print("Введена неверная операция")
 input()	
+
+
+temp = w.temperature('celsius')['temp']
+
+	answer = 'В городе ' + message.text + ' сейчас ' + w.detailed_status,", температура в районе " + str(temp) + ".\n\n"
+
+	if temp < 10:
+		answer += "Сейчас там холодно, лучше пососать хуйца"
+	elif temp < 20:
+		answer += "Холодновато там, однако. Лучше пососать хуйца"
+	else:
+		answer += "Погодка просто шик, можно соснуть хуйца!"
+
+	bot.send_message(message.chat.id, answer)
+
+bot.polling( none_stop = True )
